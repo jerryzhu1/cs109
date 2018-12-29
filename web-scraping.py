dc45e86f41e14263a32c869a94a21418e50df2ae
Siqi Zhu <szhu21@jhu.edu>
2018-12-29 17:51:59 -0500

    # testing pycharm + git work pipline

--- web-scraping.py
+++ web-scraping.py
@@ -3,24 +3,24 @@ import requests
 import re
 import pandas as pd
 
+
 # data extraction
 def html_parsing(line):
-    record = []
-    title = line.find("a", class_= re.compile('title*')).text
+    title = line.find("a", class_=re.compile('title*')).text
     time_submitted_obj = line.find("time")
     time_sub = time_submitted_obj['datetime']
     author = line.find("a", class_='author').text
-    comments = line.find("a", class_= ('bylink'))['href']
-    record = [title, time_sub, author, comments]
-    return record
+    comments = line.find("a", class_=('bylink'))['href']
+    return [title, time_sub, author, comments]
+
 
 # web crawling
 url = "https://old.reddit.com/top/"
-r = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
+r = requests.get(url, headers={'User-agent': 'your bot 0.1'})
 soup = BeautifulSoup(r.text, 'html.parser')
 
-table = soup.find("div", id = 'siteTable')
-block  = table.findAll("div",attrs={'class':re.compile("thing*")})
+table = soup.find("div", id='siteTable')
+block = table.findAll("div", attrs={'class': re.compile("thing*")})
 
 records = []
 for line in block:
